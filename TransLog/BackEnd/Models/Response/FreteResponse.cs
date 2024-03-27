e1eeb3538f0d51205f80f351277290a6e053b8ca
danillo <lopessousa2004@gmail.com>
2024-03-27 20:05:08 -0300

    feat: Alterações no back

---
+++ TransLog/BackEnd/Models/Response/FreteResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEnd.Models.Response
+{
+    public class FreteResponse
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+        public double Comprimento { get; set; }
+        public double Distancia { get; set; }
+        public double Peso { get; set; }
+    }
+}
